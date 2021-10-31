82022f96c5371649a986398bf00e03a5101987bd
Adam Tay <atay@csod.com>
2021-11-01 11:15:39 +1300

    fetch crypto data and render on table

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { Table } from "../components/Table/Table";
+import { fetchCryptoData } from "../apis";
+import { CryptoTable } from "../components/CryptoTable/CryptoTable";
+import { Crypto } from "../types";
 
-function App() {
-  return <Table />;
-}
+const cryptos = ["dogelon-mars", "saitama-inu"];
+
+const App = () => {
+  const [cryptoData, setCryptoData] = React.useState<Record<string, Crypto>>();
+
+  React.useEffect(() => {
+    const fetchCryptos = async () => {
+      const data = await fetchCryptoData(cryptos);
+      if (data) {
+        setCryptoData(data);
+      }
+    };
+
+    fetchCryptos();
+  }, []);
+
+  if (!cryptoData) {
+    return <div>Loading...</div>;
+  }
+
+  return <CryptoTable cryptos={cryptoData} />;
+};
 
 export default App;
